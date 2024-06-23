98d6c0b1c48735bfe7b4f82562436534b1a78a06
Pragati <pragati.mane@161099@gmail.com>
2024-06-23 13:17:24 +0530

    added basec services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './admins/signin/signin.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
+import { CategoryAddComponent } from './category/category-add/category-add.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { BrandListComponent } from './brand/brand-list/brand-list.component';
+import { BrandAddComponent } from './brand/brand-add/brand-add.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    ProductListComponent,
+    ProductAddComponent,
+    CategoryAddComponent,
+    CategoryListComponent,
+    BrandListComponent,
+    BrandAddComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
